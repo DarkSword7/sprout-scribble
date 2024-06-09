f9d5fa1cd689be554fb2f237ce67878c7474dc0e
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-06-09 15:51:08 +0530

    add shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default async function Home() {
   return (
     <main>
       <h1>Home</h1>
+      <Button>ShadcnUI</Button>
     </main>
   );
 }
