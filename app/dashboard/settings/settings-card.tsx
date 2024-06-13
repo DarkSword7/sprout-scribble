9ee781dd9eb86786a08b030555ef6c85f642b37c
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-06-13 20:29:06 +0530

    feat: Add upload button and dropzone components for avatar uploads

--- app/dashboard/settings/settings-card.tsx
+++ app/dashboard/settings/settings-card.tsx
@@ -30,6 +30,7 @@ import { FormError } from "@/components/auth/form-error";
 import { useState } from "react";
 import { useAction } from "next-safe-action/hooks";
 import { settings } from "@/server/actions/settings";
+import { UploadButton } from "@/app/api/uploadthing/upload";
 
 type SettingsForm = {
   session: Session;
@@ -120,6 +121,32 @@ export default function SettingsCard(session: SettingsForm) {
                         priority
                       />
                     )}
+                    <UploadButton
+                      className="scale-75 ut-button:ring-primary ut-label:bg-red-50  ut-button:bg-primary/75 hover:ut-button:bg-primary/100 ut:button:transition-all ut-button:duration-500  ut-label:hidden ut-allowed-content:hidden"
+                      endpoint="avatarUploader"
+                      onUploadBegin={() => {
+                        setAvatarUploading(true);
+                      }}
+                      onUploadError={(error) => {
+                        form.setError("image", {
+                          type: "validate",
+                          message: error.message,
+                        });
+                        setAvatarUploading(false);
+                        return;
+                      }}
+                      onClientUploadComplete={(res) => {
+                        form.setValue("image", res[0].url!);
+                        setAvatarUploading(false);
+                        return;
+                      }}
+                      content={{
+                        button({ ready }) {
+                          if (ready) return <div>Change Avatar</div>;
+                          return <div>Uploading...</div>;
+                        },
+                      }}
+                    />
                   </div>
                   <FormControl>
                     <Input
