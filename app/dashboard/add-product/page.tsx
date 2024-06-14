36b6b9474d13e1a56a37efd6857f099261ead61b
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-06-14 19:54:35 +0530

    feat: Add role-based access control to AddProduct page

--- app/dashboard/add-product/page.tsx
+++ app/dashboard/add-product/page.tsx
@@ -1,3 +1,14 @@
-export default function AddProduct() {
-  return <div>Add Product</div>;
+import { auth } from "@/server/auth";
+import { redirect } from "next/navigation";
+
+export default async function AddProduct() {
+  const session = await auth();
+
+  if (session?.user.role !== "admin") return redirect("/dashboard/settings");
+
+  return (
+    <div>
+      <h1>Add Product</h1>
+    </div>
+  );
 }
