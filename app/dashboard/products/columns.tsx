52f618372e5250ee03d17b8d9e92d4f384de55e5
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-06-16 17:17:17 +0530

    feat: Add @tanstack/react-table dependency and implement DataTable component and Column component

---
+++ app/dashboard/products/columns.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Image from "next/image";
+
+type ProductColumn = {
+  title: string;
+  price: number;
+  image: string;
+  variants: any;
+  id: number;
+};
+
+export const columns: ColumnDef<ProductColumn>[] = [
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    accessorKey: "title",
+    header: "Title",
+  },
+  {
+    accessorKey: "variants",
+    header: "Variants",
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+    cell: ({ row }) => {
+      const price = parseFloat(row.getValue("price"));
+      const formatted = new Intl.NumberFormat("en-IN", {
+        style: "currency",
+        currency: "INR",
+      }).format(price);
+      return <div className="font-medium text-xs">{formatted}</div>;
+    },
+  },
+  {
+    accessorKey: "image",
+    header: "Image",
+    cell: ({ row }) => {
+      const cellImage = row.getValue("image") as string;
+      const cellTitle = row.getValue("title") as string;
+      return (
+        <div>
+          <Image
+            src={cellImage}
+            alt={cellTitle}
+            className="rounded-md"
+            width={50}
+            height={50}
+          />
+        </div>
+      );
+    },
+  },
+  {
+    id: "action",
+    header: "Actions",
+    cell: ({ row }) => {
+      const product = row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={"ghost"} className="h-8 w-8 p-0">
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="cursor-pointer">
+            <DropdownMenuItem className="dark:focus:bg-primary focus:bg-primary/50 cursor-pointer">
+              Edit Product
+            </DropdownMenuItem>
+            <DropdownMenuItem className="dark:focus:bg-destructive focus:bg-destructive/50 cursor-pointer">
+              Delete Product
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
