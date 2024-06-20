4a84fe10505cdb70c158ebf3bae8dc8cba558aca
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-06-20 18:25:26 +0530

    feat: Update font import in layout.tsx for better performance and customization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/navigation/nav";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import Toaster from "@/components/ui/toaster";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  weight: ["400", "500", "700", "900"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,14 +23,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <body
-        suppressHydrationWarning={true}
-        className={cn("px-6 md:px-12 max-w-7xl mx-auto", `${inter.className}`)}
-      >
+      <body suppressHydrationWarning={true} className={roboto.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <Nav />
-          <Toaster />
-          {children}
+          <div className="flex-grow px-6 md:px-12 mx-auto max-w-6xl">
+            <Nav />
+            <Toaster />
+            {children}
+          </div>
         </ThemeProvider>
       </body>
     </html>
